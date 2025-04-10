bdc3ec1862c1de7e785562e539ad185c8fa91433
Dmytro Nizhnyk <nothan89@gmail.com>
2025-04-10 17:48:21 +0300

    the solution for the assignment 'js_statjs_stateful-clones'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,31 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state }; // починаємо з копії початкового стану
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    } else if (action.type === 'addProperties') {
+      newState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      newState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
