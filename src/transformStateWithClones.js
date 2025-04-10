b810c8fc001dcb5ea7c51f5231b4c9145fc02b51
Dmytro Nizhnyk <nothan89@gmail.com>
2025-04-10 18:07:23 +0300

    The changes have been incorporated into 'js_statjs_stateful-clones'

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -9,23 +9,26 @@
 
 function transformStateWithClones(state, actions) {
   const result = [];
-  let currentState = { ...state }; // починаємо з копії початкового стану
+  let currentState = { ...state };
 
   for (const action of actions) {
     let newState;
 
-    if (action.type === 'clear') {
-      newState = {};
-    } else if (action.type === 'addProperties') {
-      newState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      newState = { ...currentState };
+    switch (action.type) {
+      case 'addProperties':
+        newState = { ...currentState, ...action.extraData };
+        break;
+      case 'removeProperties':
+        newState = { ...currentState };
 
-      for (const key of action.keysToRemove) {
-        delete newState[key];
-      }
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+      case 'clear':
+        newState = {};
+        break;
     }
-
     result.push(newState);
     currentState = newState;
   }
